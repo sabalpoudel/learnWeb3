846462d4d2279d127062858daa1c0dd1fc520ac9
sabal poudel <sabalenfiniy@gmail.com>
2024-01-13 20:23:44 +0545

    NFT-col: fixed Specify the arguments or mark CryptoDevs as abstract error by adding Ownable(msg.sender)

--- Freshman/NFT-collection/contracts/CryptoDevs.sol
+++ Freshman/NFT-collection/contracts/CryptoDevs.sol
@@ -1,24 +1,23 @@
-// SPDX-License-Identefier : MIT
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./Whitelist.sol";
 
-abstract contract CryptoDevs is ERC721Enumerable, Ownable {
-    uint256 public constant _price = 0.0001 ether;
+contract CryptoDevs is ERC721Enumerable, Ownable {
+    uint256 public constant _price = 0.001 ether; //  _price is the price of one Crypto Dev NFT
+    uint256 public constant maxTokenIds = 20; // Max number of CryptoDevs that can ever exist
+    WhiteList whitelist; // Whitelist contract instance
 
-    uint256 public constant maxTokenIds = 20;
-
-    WhiteList wl;
-
-    uint256 public reservedTokens;
+    uint256 public reservedTokens; // Number of tokens reserved for whitelisted members
     uint256 public reservedTokensClaimed = 0;
 
-    constructor(address wlContract) ERC721("Crypto Devs", "CD") {
-        wl = WhiteList(wlContract);
-        reservedTokens = wl.maxWLAddresses();
+    constructor(
+        address whitelistContract
+    ) ERC721("Crypto Devs", "CD") Ownable(msg.sender) {
+        whitelist = WhiteList(whitelistContract);
+        reservedTokens = whitelist.maxWLAddresses();
     }
 
     function mint() public payable {
@@ -26,20 +25,26 @@ abstract contract CryptoDevs is ERC721Enumerable, Ownable {
         require(
             totalSupply() + reservedTokens - reservedTokensClaimed <
                 maxTokenIds,
-            "EXCEEDED MAX SUPPLY"
+            "EXCEEDED_MAX_SUPPLY"
         );
 
-        if (wl.wLAddresses(msg.sender) && msg.value < _price) {
-            require(balanceOf(msg.sender) == 0, "Already Owned");
+        // If user is part of the whitelist, make sure there is still reserved tokens left
+        if (whitelist.wLAddresses(msg.sender) && msg.value < _price) {
+            // Make sure user doesn't already own an NFT
+            require(balanceOf(msg.sender) == 0, "ALREADY_OWNED");
             reservedTokensClaimed += 1;
         } else {
-            require(msg.value >= _price, "NOT ENOUGH ETHER");
+            // If user is not part of the whitelist, make sure they have sent enough ETH
+            require(msg.value >= _price, "NOT_ENOUGH_ETHER");
         }
-
         uint256 tokenId = totalSupply();
         _safeMint(msg.sender, tokenId);
     }
 
+    /**
+     * @dev withdraw sends all the ether in the contract
+     * to the owner of the contract
+     */
     function withdraw() public onlyOwner {
         address _owner = owner();
         uint256 amount = address(this).balance;
