7695a8c50461e4a2e0fb9720cf9105e3e79689af
sabal poudel <sabalenfiniy@gmail.com>
2024-01-16 21:48:59 +0545

    DAO: fixed app crash by badf hack adding use client at top of root layout

--- Freshman/dao/frontend/app/layout.js
+++ Freshman/dao/frontend/app/layout.js
@@ -1,3 +1,4 @@
+"use client";
 import "@rainbow-me/rainbowkit/styles.css";
 import "../styles/Home.module.css";
 
@@ -5,23 +6,38 @@ import { sepolia } from "wagmi/chains";
 import { Inter } from "next/font/google";
 import { publicProvider } from "wagmi/providers/public";
 import { configureChains, createConfig, WagmiConfig } from "wagmi";
-import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import {
+  getDefaultWallets,
+  RainbowKitProvider,
+  // connectorsForWallets,
+} from "@rainbow-me/rainbowkit";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "CryptoDevs DAO",
-  description:
-    "Build an NFT-powered fully on-chain DAO to invest in NFT collections as a group",
+const demoAppInfo = {
+  appName: '"CryptoDevs DAO',
 };
 
 const { chains, publicClient } = configureChains([sepolia], [publicProvider()]);
 
+// const { wallets } = getDefaultWallets({
 const { connectors } = getDefaultWallets({
   appName: "CryptoDevs DAO",
   projectId: "46c1c9b226e62517bcb73f72f86e2243",
   chains,
 });
+
+// const connectors = connectorsForWallets([
+//   ...wallets,
+//   {
+//     groupName: "Other",
+//     wallets: [
+//       argentWallet({ projectId, chains }),
+//       trustWallet({ projectId, chains }),
+//       ledgerWallet({ projectId, chains }),
+//     ],
+//   },
+// ]);
 
 const wagmiConfig = createConfig({
   autoConnect: true,
@@ -34,7 +50,9 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <WagmiConfig config={wagmiConfig}>
-          <RainbowKitProvider chains={chains}>{children}</RainbowKitProvider>
+          <RainbowKitProvider appInfo={demoAppInfo} chains={chains}>
+            {children}
+          </RainbowKitProvider>
         </WagmiConfig>
       </body>
     </html>
